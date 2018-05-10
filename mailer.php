37799c6467590204a115cf007e2f1debe37fa130
akshayvinchurkar <akshay.vinchurkar@sarpstechnologies.in>
2018-05-10 10:50:07 +0530

    basic touch indentation

--- mailer.php
+++ mailer.php
@@ -1,10 +1,11 @@
 <?php
-
     // Only process POST reqeusts.
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        
         // Get the form fields and remove whitespace.
         $name = strip_tags(trim($_POST["name"]));
-				$name = str_replace(array("\r","\n"),array(" "," "),$name);
+		$name = str_replace(array("\r","\n"),array(" "," "),$name);
+        
         $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
         $message = trim($_POST["message"]);
 
